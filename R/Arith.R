34b2417f56b12d37272b6cdc360711fb54274f5e
stla <laurent_step@yahoo.fr>
2022-11-08 18:48:04 +0100

    arith lazyNumber numeric

--- R/Arith.R
+++ R/Arith.R
@@ -43,28 +43,31 @@ lazyNumber_arith_lazyNumber <- function(e1, e2) {
   )
 }
 
-# "onion_arith_numeric" <- function(e1,e2){  # e1 onion, e2 numeric
-#   switch(.Generic,
-#          "+" = onion_plus_numeric (e1,  e2),
-#          "-" = onion_plus_numeric (e1, -e2),
-#          "*" = onion_prod_numeric (e1,  e2),
-#          "/" = onion_prod_numeric (e1,1/e2),
-#          "^" = onion_power_numeric(e1,  e2),
-#          stop(gettextf("binary operator %s not defined for onions", dQuote(.Generic)))
-#   )
-# }
-# 
-# "numeric_arith_onion" <- function(e1,e2){ # e1 numeric, e2 onion
-#   switch(.Generic,
-#          "+" = onion_plus_numeric(e2,  e1),
-#          "-" = onion_plus_numeric(-e2, e1),
-#          "*" = onion_prod_numeric(e2,  e1),
-#          "/" = onion_prod_numeric(onion_inverse(e2),e1),  # onions commute with numeric multiplication
-#          "^" = stop("x^onion not defined"),
-#          stop(gettextf("binary operator %s not defined for onions", dQuote(.Generic)))
-#   )
-# }
+lazyNumber_arith_numeric <- function(e1, e2) {
+  switch(
+    .Generic,
+    "+" = e1 + as.lazyNumber(e2),
+    "-" = e1 - as.lazyNumber(e2),
+    "*" = e1 * as.lazyNumber(e2),
+    "/" = e1 / as.lazyNumber(e2),
+    stop(gettextf(
+      "Binary operator %s not defined for lazy numbers.", dQuote(.Generic)
+    ))
+  )
+}
 
+numeric_arith_lazyNumber <- function(e1, e2) {
+  switch(
+    .Generic,
+    "+" = as.lazyNumber(e1) + e2,
+    "-" = as.lazyNumber(e1) - e2,
+    "*" = as.lazyNumber(e1) * e2,
+    "/" = as.lazyNumber(e1) / e2,
+    stop(gettextf(
+      "Binary operator %s not defined for lazy numbers.", dQuote(.Generic)
+    ))
+  )
+}
 
 setMethod(
   "Arith", 
@@ -72,5 +75,14 @@ setMethod(
   lazyNumber_arith_lazyNumber
 )
 
-# setMethod("Arith",signature(e1 = "onion"  , e2="numeric"),   onion_arith_numeric)
-# setMethod("Arith",signature(e1 = "numeric", e2="onion"  ), numeric_arith_onion  )
+setMethod(
+  "Arith", 
+  signature(e1 = "lazyNumber", e2 = "numeric"), 
+  lazyNumber_arith_numeric
+)
+
+setMethod(
+  "Arith", 
+  signature(e1 = "numeric", e2 = "lazyNumber"), 
+  numeric_arith_lazyNumber
+)
--- inst/essais/essai_Euler.R
+++ inst/essais/essai_Euler.R
@@ -4,8 +4,9 @@ n <- 170
 x <- seq(1, n, by = 1)
 y <- seq(3, 2*n+1, by = 2)
 
-sum(cumprod(x) / cumprod(y))
+2 * (1 + sum(cumprod(x) / cumprod(y)))
+lnumber <- 2 * (1 + sum(cumprod(lazynb(x)) / cumprod(lazynb(y))))
 
-as.double(sum(cumprod(lazynb(x)) / cumprod(lazynb(y))))
+as.double(lnumber)
 
-print(intervals(sum(cumprod(lazynb(x)) / cumprod(lazynb(y)))), digits = 20)
+print(intervals(lnumber), digits = 20)
