7fd6f864f6bc3bad9da5cd03ba6a66b5de145ee0
stla <laurent_step@yahoo.fr>
2022-11-10 01:21:39 +0100

    replace

--- R/Extract.R
+++ R/Extract.R
@@ -1,11 +1,12 @@
 #' @name ExtractOrReplace
-#' @aliases [,lazyVector,numeric,missing-method
+#' @aliases [,lazyVector,numeric,missing-method [<-,lazyVector,numeric,missing,lazyVector-method
 #' @title Extract/replace in a lazy vector
 #' @description Extract or replace elements in a lazy vector.
 #' @param x a \code{lazyVector} object
 #' @param i indices
 #' @param j nothing
 #' @param drop ignored
+#' @param value a \code{lazyVector} object
 setMethod(
   "[", 
   signature("lazyVector", i = "numeric", j = "missing", drop = "ANY"), 
@@ -16,5 +17,22 @@ setMethod(
     }
     lvx <- lazyExtract(x@xptr, as.integer(i))
     new("lazyVector", xptr = lvx, length = length(i))
+  }
+)
+
+#' @rdname ExtractOrReplace
+setReplaceMethod(
+  "[", 
+  signature("lazyVector", i = "numeric", j = "missing", value = "lazyVector"), 
+  function(x, i, j, value) {
+    stopifnot(isIndexVector(i))
+    if(any(i) > x@length) {
+      stop("Too large index.")
+    }
+    if(length(i) != value@length) {
+      stop("Incompatible lengths.")
+    }
+    lvx <- lazyReplace(x@xptr, as.integer(i), value@xptr)
+    new("lazyVector", xptr = lvx, length = x@length)
   }
 )
