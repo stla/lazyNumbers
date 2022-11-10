23b0462824b305624cec89cf0e312df02e3ff2d8
stla <laurent_step@yahoo.fr>
2022-11-10 12:00:58 +0100

    cbind rbind

--- R/Concat.R
+++ R/Concat.R
@@ -13,13 +13,63 @@ c_lv <- function(lv1, lv2) {
 setMethod(
   "c",
   signature(x = "lazyVector"),
-  function(x, ...){
+  function(x, ...) {
     if(nargs() == 1L) {
       x
     } else if(nargs() == 2L) {
       c_lv(x, ...)
     } else {
       c_lv(x, Recall(...))
+    }
+  }
+)
+
+cbind_lm <- function(lm1, lm2) {
+  stopifnot(lm1@nrow == lm2@nrow)
+  xptr <- lazyCbind(lm1@xptr, lm2@xptr)
+  n <- lm1@ncol + lm2@ncol
+  new("lazyMatrix", xptr = xptr, nrow = lm1@nrow, ncol = n)
+}
+
+rbind_lm <- function(lm1, lm2) {
+  stopifnot(lm1@ncol == lm2@ncol)
+  xptr <- lazyRbind(lm1@xptr, lm2@xptr)
+  m <- lm1@nrow + lm2@nrow
+  new("lazyMatrix", xptr = xptr, nrow = m, ncol = lm1@ncol)
+}
+
+#' @name bind-lazyMatrices
+#' @aliases cbind,lazyMatrix-method rbind,lazyMatrix-method
+#' @title Concatenation of lazy matrices
+#' @description Concatenate two or more \code{lazyMatrix} objects.
+#' @param ... some \code{lazyMatrix} objects
+#' @param deparse.level ignored
+setMethod(
+  "cbind",
+  signature(),
+  function(..., deparse.level) {
+    if(nargs() == 1L) {
+      identity(...)
+    } else if(nargs() == 2L) {
+      cbind_lm(...)
+    } else {
+      xs <- list(...)
+      cbind_lm(xs[[1L]], do.call(Recall, xs[-1L]))
+    }
+  }
+)
+
+#' @rdname bind-lazyMatrices
+setMethod(
+  "rbind",
+  signature(),
+  function(..., deparse.level) {
+    if(nargs() == 1L) {
+      identity(...)
+    } else if(nargs() == 2L) {
+      rbind_lm(...)
+    } else {
+      rbind_lm(xs[[1L]], do.call(Recall, xs[-1L]))
     }
   }
 )
